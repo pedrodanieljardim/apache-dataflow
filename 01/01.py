66d5517ccd1948c0c3ea5b19b7ab53c4a68377dc
Pedro Daniel Jardim <pedrodanieljardim@gmail.com>
2023-03-20 21:33:11 -0300

    feat: pipeline construido apartir de csv

--- 01/01.py
+++ 01/01.py
@@ -4,8 +4,20 @@ import apache_beam as beam
 # Definicao de pipeline
 p1 = beam.Pipeline()
 
+# LER ARQUIVO E EXCLUIR CABECALHO
+
 pCollection = (
   p1
+
+  # STEP 1
+  | "Importar Dados" >> beam.io.ReadFromText("voos_sample.csv", skip_header_lines=1)
+
+  # STEP 2
+  | "Separar por Virgulas" >> beam.Map(lambda record: record.split(","))
+
+  # STEP 3
+  | "Mostrar resultados" >> beam.Map(print)
+
 )
 
 p1.run()
